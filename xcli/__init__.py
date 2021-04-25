a6d367d50b0dab5d00ad95a1c2bc52de4d7e0b2a
Ian Fisher <iafisher@fastmail.com>
2021-04-25 14:02:00 -0500

    Remove argument parsing code
    In favor of the click library

--- xcli/__init__.py
+++ xcli/__init__.py
@@ -2,4 +2,3 @@ from . import colors
 from ._exception import XCliError
 from ._input import confirm, input2
 from ._output import Table, lorem_ipsum
-from ._parser import Arg, ArgumentParser, Flag, ParsedArguments, Subcommand
